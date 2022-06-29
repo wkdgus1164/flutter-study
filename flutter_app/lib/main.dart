310d16d2fef5990401b178d5edd2dbedd6803bab
wkdgus1164 <wkdgus1164@naver.com>
2022-06-30 01:41:17 +0900

    update: SingleChildScrollView (#1)

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -25,88 +25,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello World'),
+        title: const Text('Hello World'),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 100,
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.all(8.0),
-              ),
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 100,
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.all(8.0),
-              ),
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 100,
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.all(8.0),
-              ),
-            ],
-          ),
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
-          ),
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
-          ),
-          Container(
-            color: Colors.red,
-            width: 100,
-            height: 100,
-            padding: const EdgeInsets.all(8.0),
-            margin: const EdgeInsets.all(8.0),
-          ),
-          Stack(
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 100,
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.all(8.0),
-              ),
-              Container(
-                color: Colors.blue,
-                width: 80,
-                height: 80,
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.all(8.0),
-              ),
-              Container(
-                color: Colors.green,
-                width: 60,
-                height: 60,
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.all(8.0),
-              ),
-            ],
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: ListBody(children: items.map((e) => Text('$e')).toList()),
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {

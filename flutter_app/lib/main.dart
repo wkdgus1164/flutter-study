8940fabc7046e2b414159ba54899d4d6b5a25d10
wkdgus1164 <wkdgus1164@naver.com>
2022-06-30 19:19:15 +0900

    update: CircularProgressIndicator (#1)

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -26,7 +26,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text("App"),
       ),
-      body: Image.network('http://bit.ly/2Pvz4t8'),
+      body: const CircularProgressIndicator(),
     );
   }
 }

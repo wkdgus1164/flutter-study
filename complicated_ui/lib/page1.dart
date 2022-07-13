ed6e84af6426a24f7cdf5b8bf638f966477386d4
wkdgus1164 <wkdgus1164@naver.com>
2022-07-14 01:53:10 +0900

    feat: 1페이지 - 상단 메뉴 생성 (#4)

--- complicated_ui/lib/page1.dart
+++ complicated_ui/lib/page1.dart
@@ -5,11 +5,87 @@ class Page1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        '홈 페이지',
-        style: TextStyle(fontSize: 40),
-      ),
+    return Column(
+      children: <Widget>[_buildTop(), _buildMiddle(), _buildBottom()],
     );
+  }
+
+  Widget _buildTop() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: const <Widget>[
+                Icon(Icons.local_taxi, size: 40),
+                Text("택시")
+              ],
+            ),
+            Column(
+              children: const <Widget>[
+                Icon(Icons.local_taxi, size: 40),
+                Text("블랙")
+              ],
+            ),
+            Column(
+              children: const <Widget>[
+                Icon(Icons.local_taxi, size: 40),
+                Text("바이크")
+              ],
+            ),
+            Column(
+              children: const <Widget>[
+                Icon(Icons.local_taxi, size: 40),
+                Text("대리")
+              ],
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: const <Widget>[
+                Icon(Icons.local_taxi, size: 40),
+                Text("택시")
+              ],
+            ),
+            Column(
+              children: const <Widget>[
+                Icon(Icons.local_taxi, size: 40),
+                Text("블랙")
+              ],
+            ),
+            Column(
+              children: const <Widget>[
+                Icon(Icons.local_taxi, size: 40),
+                Text("바이크")
+              ],
+            ),
+            Opacity(
+              opacity: 0,
+              child: Column(
+                children: const <Widget>[
+                  Icon(Icons.local_taxi, size: 40),
+                  Text("대리")
+                ],
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _buildMiddle() {
+    return const Text('Middle');
+  }
+
+  Widget _buildBottom() {
+    return const Text('Bottom');
   }
 }

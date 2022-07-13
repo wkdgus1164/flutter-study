3cbba6ef7b0f897d239df47836855c05cb3140ed
wkdgus1164 <wkdgus1164@naver.com>
2022-07-14 02:55:39 +0900

    feat: 결과 화면 작성 (#6)

--- health_calculator/lib/bmi_result.dart
+++ health_calculator/lib/bmi_result.dart
@@ -1,10 +1,25 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class BmiResult extends StatelessWidget {
-  const BmiResult({Key? key}) : super(key: key);
+  final double height;
+  final double weight;
+
+  const BmiResult(this.height, this.weight);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text("비만도 계산기")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text("정상", style: TextStyle(fontSize: 36)),
+            SizedBox(height: 16),
+            Icon(Icons.sentiment_satisfied, color: Colors.green, size: 100)
+          ],
+        ),
+      ),
+    )
   }
 }
